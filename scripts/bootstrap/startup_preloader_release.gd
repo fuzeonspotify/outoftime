1198d93dd934ee425738692fe2ddbcd059a3a510
fuzeonspotify <83621025+fuzeonspotify@users.noreply.github.com>
2026-07-31 05:40:50 -0700

    Expose the complete rigged player model

--- scripts/bootstrap/startup_preloader_release.gd
+++ scripts/bootstrap/startup_preloader_release.gd
@@ -1,6 +1,12 @@
 extends "res://scripts/bootstrap/startup_preloader.gd"
 
 
+func get_player_character_prototype() -> Node3D:
+	if _character_library == null:
+		return null
+	return _character_library.call("get_player_character_prototype") as Node3D
+
+
 func _prepare_all() -> void:
 	if _preparing or _ready_for_gameplay:
 		return
